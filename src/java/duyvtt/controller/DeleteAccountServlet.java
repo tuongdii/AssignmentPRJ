0b75295b8db4b89aa873593bba20e6be2d847fcf
tuongdii <vothituongduy20@gmail.com>
2022-02-07 21:40:34 +0700

    add filter

---
+++ src/java/duyvtt/controller/DeleteAccountServlet.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package duyvtt.controller;
+
+import duyvtt.registration.RegistrationDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.Properties;
+import javax.naming.NamingException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author DELL
+ */
+
+public class DeleteAccountServlet extends HttpServlet {
+    private final String ERROR_PAGE = "errors.html";
+    private final String SEARCH_PAGE = "searchPageAdmin";
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String username = request.getParameter("username");
+        String searchValue = request.getParameter("lastSearchValue");
+        System.out.println("User " + username);
+        System.out.println("value " + searchValue);
+        String url = ERROR_PAGE;
+        try  {
+            //call DAO
+            RegistrationDAO dao = new RegistrationDAO();
+            boolean result = dao.deleteAccount(username);
+            if(result){
+                //.call previous function again
+                url = SEARCH_PAGE;
+                ServletContext context = request.getServletContext();
+                Properties siteMapProp = (Properties) context.getAttribute("SITE_MAP");
+                url = siteMapProp.getProperty(url);
+            }//end if delete successfully
+        }catch(SQLException e){
+            e.printStackTrace();
+        }catch(NamingException e){
+            e.printStackTrace();
+        }finally{
+            url = url + "?txtSearchValue=" + searchValue;
+            RequestDispatcher rd = request.getRequestDispatcher(url);
+            rd.forward(request, response);
+            out.close();
+        }
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
